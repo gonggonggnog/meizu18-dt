34f44ff559eee2559c8dae1073ff9b6dac2475e3
gonggonggnog <30695669+gonggonggnog@users.noreply.github.com>
2023-04-05 15:11:11 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_meizu18.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_meizu18-user \
+    omni_meizu18-userdebug \
+    omni_meizu18-eng
